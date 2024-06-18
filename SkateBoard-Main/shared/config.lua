7c4b95d45b72cc07b290cd1f070debdaf2557d2f
𐌀𐋄𐌋 👽 <axel.martin2809@gmx.fr>
2024-06-19 01:57:57 +0200

    Delete SkateBoard-Main directory

--- SkateBoard-Main/shared/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {}
-Config.Debug = false -- Vrai / Faux pour le système de débogage
-
--- Paramètres
-Config.ItemName = 'skateboard'
-Config.MaxSpeedKmh = 40 -- Cela ne change pas grand-chose à moins d'obtenir un boost d'une manière ou d'une autre.
-Config.maxJumpHeigh = 5.0 -- Nous vous suggérons de ne pas trop toucher à cela (Et oui, vous pouvez sauter très haut).
-Config.LoseConnectionDistance = 2.0 -- Il s'agit de la distance entre vous et le skateboard (Ne touchez pas à cela, sauf si vous savez ce que vous faites).
